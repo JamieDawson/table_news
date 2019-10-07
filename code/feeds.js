78865cb7d3ee7275373889496201769cd5fd4eda
Jamie Dawson <jadawson@e1z3r6p8.42.us.org>
2019-10-06 19:31:26 -0700

    Added hints, feeds, training that works

--- code/feeds.js
+++ code/feeds.js
@@ -4,15 +4,21 @@
 // Tags: What the user can say to automatically call certain feeds
 module.exports = module.exports = [
   {
-    url: 'https://thefantasyfootballers.libsyn.com/fantasyfootball',
-    urlText: 'Click here to see the full story on thefantasyfootballers.libsyn.com',
-    text: 'Example Podcast',
-    tags: ['the fantasy footballers']
+    url: 'https://www.boardgamegeek.com/rss/blog/1',
+    urlText: 'Click here to see the full story on boardgamegeek.com',
+    text: 'Board Game Geek',
+    tags: ['board game geek',]
+  },
+    {
+    url: 'https://www.dicetower.com/master-feed.xml/feed',
+    urlText: 'Click here to see the full story on dicetower.com',
+    text: 'The Dice Tower',
+    tags: ['The Dice Tower', "Dice Tower"]
   },
   {
-    url: 'https://www.ketoconnect.net/feed/',
-    urlText: 'Click here to see the full story on espn.com',
-    text: 'Example Text',
-    tags: ['sports', 'espn']
+    url: 'https://www.boardgamequest.com/feed',
+    urlText: 'Click here to see the full story on boardgamequest.com',
+    text: 'Board Game Quest',
+    tags: ['board game quest']
   },
 ]
